46dc315ef20bfbccd41a9db212ebc294e90fb4bd
danfujii <54048508+danfujii@users.noreply.github.com>
2019-09-15 22:51:53 -0700

    Add files via upload

--- py_bank-main.py
+++ py_bank-main.py
@@ -1,71 +1,53 @@
 import os
 import csv
 
-bank_csv = os.path.join("Resources", "budget_data.csv")
+csvpath = os.path.join("Resources", "budget_data.csv")
 
-
-with open(bank_csv, newline='') as csvfile:
-    bank_data = csv.reader(csvfile, delimiter=',')
-#    print(bank_data)
-    next(bank_data, None)
-
-    months = sum((1 for row in bank_data))
-
+with open(csvpath, newline="") as csvfile:
+    
+    csvreader = csv.reader(csvfile, delimiter=",")
+    csv_header = next(csvfile)
+ 
+    months = []
+    profit_loss = []
+    differences = []
+    greatest_increase_date = ""
+    greatest_decrease_date = ""
+    
+    for row in csvreader:
+        months.append(row[0])   
+        profit_loss.append(int(row[1]))
+      
+    
     print("Financial Analysis\n"
         "----------------------------")
+    print(f"Total Months: {len(months)}\n"
+    f"Total: $ {sum(profit_loss)}")
+    
 
-    print(f"Total Months: {months}")
+    for i in range(1, len(profit_loss)):
+        
+        differences.append(profit_loss[i] - profit_loss[i-1])
+        
+        avg_change = sum(differences) / len(differences)
+        
+        greatest_increase = max(differences)
+        greatest_increase_date = str(months[differences.index(max(differences))])
+        
+        greatest_decrease = min(differences)
+        greatest_decrease_date = str(months[differences.index(min(differences))])
+  
+    print(f"Average Change: $ {round(avg_change,2)}")  
+    print(f"Greatest Increase in Profits: $ {greatest_increase_date} $ {greatest_increase}")
+    print(f"Greatest Decrease in Profits: $ {greatest_decrease_date} $ {greatest_decrease}")
 
-with open(bank_csv, newline='') as csvfile:
-    bank_data = csv.reader(csvfile, delimiter=',')
-    next(bank_data, None)    
-
-    total = sum(int(row[1]) for row in bank_data)
-
-    print(f"Total: $ {total}")
-
-with open(bank_csv, newline='') as csvfile:
-    bank_data = csv.reader(csvfile, delimiter=',')
-    next(bank_data, None)
-
-    avg_change = total / months
-
-    print(f"Average Change: $ {avg_change}")
-
-with open(bank_csv, newline='') as csvfile:
-    bank_data = csv.reader(csvfile, delimiter=',')
-    next(bank_data, None)
-    max_profits = max(row[1] for row in bank_data)
-
-    print(f"Greatest Increase in Profits: $ {max_profits}")    
-
-with open(bank_csv, newline='') as csvfile:
-    bank_data = csv.reader(csvfile, delimiter=',')
-    next(bank_data, None)
-
-    min_profits = min(row[1] for row in bank_data)
-
-    print(f"Greatest Decrease in Profits: $ {min_profits}")
-
-
-
-
-#with open(bank_csv, newline='') as csvfile:
-#    bank_data = csv.reader(csvfile, delimiter=',')
-#    next(bank_data, None)
-
- #   change_rev = (int(row[1][1]) for row in bank_data)
-
-  #  avg_change = change_rev / months
-
-   # print(f"Average Change: $ {avg_change}")
 
 text_file = open("bank_summary.txt", "w")
 text_file.write("Financial Analysis\n"
         "----------------------------\n"
-        f"Total Months: {months}\n"
-        f"Total: $ {total}\n"
-        f"Average Change: $ {avg_change}\n"
-        f"Greatest Increase in Profits: $ {max_profits}\n"
-        f"Greatest Decrease in Profits: $ {min_profits}")
+        f"Total Months: {len(months)}\n"
+        f"Total: $ {sum(profit_loss)}\n"
+        f"Average Change: $ {round(avg_change,2)}\n"
+        f"Greatest Increase in Profits: $ {greatest_increase_date} $ {greatest_increase}\n"
+        f"Greatest Decrease in Profits: $ {greatest_decrease_date} $ {greatest_decrease}")
 text_file.close()
